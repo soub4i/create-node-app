4e51a51264a36ef6befe81a0376cb683454ef15f
Abderrhim Soubai <gx.coder@gmail.com>
2019-05-14 01:37:07 +0100

    Add new command feature

--- index.js
+++ index.js
@@ -2,30 +2,38 @@
 
 const program = require("commander");
 const inquirer = require("inquirer");
-const figlet = require("figlet");
-const { generateFile, repoCloner } = require("./lib/creator");
+const { loader, generateFile, gitHandler } = require("./lib/creator");
 
-figlet("create-node-app", function(err, data) {
-  if (err) {
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
+loader();
 
 program
-  .option("-t,--ts")
-  .option("-j,--js")
+
   .command("generate <type> <file>")
   .alias("g")
   .description("Generate a file")
-  .action((type, filename) => generateFile(type, filename))
+  .action(async (type, filename) => {
+    await generateFile(type, filename);
+  });
+
+program
+
   .command("new <project>")
-  .action(project => repoCloner(project));
+  .alias("g")
+
+  .description("create a new project a file")
+
+  .action(project => {
+    gitHandler(project);
+  });
+
+program
+  .option("--typescript")
+  .option("--javascript")
+  .description("Minimalist CLI to speedUP your node projects");
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
   process.exit();
 }
 
-program.parse(process.argv);
+setTimeout(_ => program.parse(process.argv), 50);
